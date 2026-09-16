95fc0933776c51750c50689e44e2f4997102fc8f
agotrancoso-create <agotrancoso@gmail.com>
2026-09-15 23:27:26 -0300

    Aplica ajuste mestre visual

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './ago-paleta-final.css';
 import './ago-beneficios-referencia.css';
 import './ago-hero-sem-borda.css';
 import './ago-proporcao-final.css';
+import './ago-ajuste-mestre.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
