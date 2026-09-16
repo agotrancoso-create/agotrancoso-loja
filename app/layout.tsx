720899361fc0a79ed856caa86e1bc19732d1730b
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 10:31:20 -0300

    feat: load definitive visual layer last

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ import './ago-ajuste-final-sessoes.css';
 import './ago-paleta-coesa-final.css';
 import './ago-direcao-final.css';
 import './ago-sem-verde-final.css';
+import './ago-prompt-mestre-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
