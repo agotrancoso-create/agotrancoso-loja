fe622f6cef2afce148d3a72000af6d9b2e7528f8
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 16:07:02 -0300

    force: apply final universal visual layer after all legacy styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ import './ago-sem-linhas-proporcional.css';
 import './ago-ajuste-proporcao-final.css';
 import './ago-mobile-preenchido-final.css';
 import './ago-unificacao-total.css';
+import './ago-force-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
