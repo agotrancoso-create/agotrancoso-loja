cc18004ba39ab0d20455fce90b8ee7fbf9de77f5
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 16:43:33 -0300

    Carrega estilo mestre final

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ import './ago-force-final.css';
 import './ago-auditoria-visual-final.css';
 import './ago-rodape-tipografia-final.css';
 import './ago-neuro-conversao-final.css';
+import './ago-mestre-final-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
