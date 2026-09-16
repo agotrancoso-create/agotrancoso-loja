2e70a28150afd470e916cd2800e459fb490c3412
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 10:49:14 -0300

    feat: carrega direcao visual artesanal por ultimo

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ import './ago-paleta-coesa-final.css';
 import './ago-direcao-final.css';
 import './ago-sem-verde-final.css';
 import './ago-prompt-mestre-final.css';
+import './ago-direcao-artesanal-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
