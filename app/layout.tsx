f24bf2bb3449c7679060c4c3e90f8b4da87ac181
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 13:22:46 -0300

    fix: aplica camada final de layout terroso

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ import './ago-prompt-mestre-final.css';
 import './ago-direcao-artesanal-2026.css';
 import './ago-redesign-2026.css';
 import './ago-centralizado-terra-final.css';
+import './ago-layout-final-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
