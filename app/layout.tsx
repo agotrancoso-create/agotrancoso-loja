7f0f3d546f3f8f4c8bc32de12d385dcd4ad90884
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 15:40:31 -0300

    style: aplica camada final proporcional sem linhas

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ import './ago-layout-final-2026.css';
 import './ago-direcao-proporcional-terra-2026.css';
 import './ago-paleta-rodape-final.css';
 import './ago-direcao-mestre-2026.css';
+import './ago-sem-linhas-proporcional.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
