1f8cab6a5e9db0060b1f8c0b3203a82ad55f5429
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 16:19:10 -0300

    Carrega tipografia final do rodapé como última camada

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ import './ago-mobile-preenchido-final.css';
 import './ago-unificacao-total.css';
 import './ago-force-final.css';
 import './ago-auditoria-visual-final.css';
+import './ago-rodape-tipografia-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
