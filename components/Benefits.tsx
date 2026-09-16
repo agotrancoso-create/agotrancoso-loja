d2e8717a492de5727d864cccf84dae94cab62b91
agotrancoso-create <agotrancoso@gmail.com>
2026-09-15 22:14:45 -0300

    Corrige símbolos dos benefícios

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -1,43 +1,43 @@
 const benefits = [
   {
     title: 'feito à mão',
-    text: 'cuidado e tradição em cada detalhe.',
+    text: 'Cada peça é única e artesanal.',
     icon: (
       <svg viewBox="0 0 48 48" aria-hidden="true">
-        <path d="M12 31c1.5-5.7 4.2-13.9 8.1-13.9 2.1 0 3.1 2.1 2.2 4.5l-1 2.7c-.5 1.4.2 2.5 1.6 2.5h2.4c2.6 0 4.7 1.9 4.7 4.4 0 4-3.4 6.8-8 6.8h-3.1c-3.1 0-5.8-1.8-7.1-4.7l-1.1-2.4c-.6-1.5.3-3 1.8-3 .8 0 1.6.5 2 1.3l1.6 3.1" />
-        <path d="M29.5 18.8c.8-2.1 2.1-3.6 3.9-4.7M34.4 21c2-.9 3.7-.9 5.3-.4" />
+        <path d="M17 37c-2.5-2.7-4-6.2-4-10V15.5c0-1.7 1.1-3 2.5-3s2.5 1.3 2.5 3V24" />
+        <path d="M18 24V10.5c0-1.7 1.1-3 2.5-3s2.5 1.3 2.5 3V24" />
+        <path d="M23 24V9.5c0-1.7 1.1-3 2.5-3S28 7.8 28 9.5V24" />
+        <path d="M28 24V12c0-1.7 1.1-3 2.5-3S33 10.3 33 12v15c0 7-3.8 12-10.5 12H20c-4.2 0-7.5-2.1-9.1-5.8L9 28.8c-.6-1.5.1-3.1 1.5-3.7 1.4-.6 3 .1 3.7 1.5l1.5 3.2" />
       </svg>
     ),
   },
   {
     title: 'peças exclusivas',
-    text: 'escolhas especiais para quem valoriza o feito à mão.',
+    text: 'Design autoral inspirado na cultura brasileira.',
     icon: (
       <svg viewBox="0 0 48 48" aria-hidden="true">
-        <path d="M17 11h14l-1.5 7c-.2 1-.9 1.8-1.8 2.4v10.2c0 2.1 1.4 3.8 3.4 4.5v2.1H16.9v-2.1c2-.7 3.4-2.4 3.4-4.5V20.4c-.9-.6-1.6-1.4-1.8-2.4L17 11Z" />
-        <path d="M15 11h18M20 18c2.2 1.2 5.8 1.2 8 0M19 32h10" />
+        <path d="M24 39S9 29.2 9 18.6C9 13.2 12.7 10 17.2 10c2.9 0 5.4 1.6 6.8 4 1.4-2.4 3.9-4 6.8-4C35.3 10 39 13.2 39 18.6 39 29.2 24 39 24 39Z" />
       </svg>
     ),
   },
   {
     title: 'inspiração brasileira',
-    text: 'cores, formas e símbolos da nossa terra.',
+    text: 'Cores, formas e símbolos da nossa terra.',
     icon: (
       <svg viewBox="0 0 48 48" aria-hidden="true">
-        <circle cx="24" cy="24" r="7" />
-        <path d="M24 6v5M24 37v5M6 24h5M37 24h5M11.3 11.3l3.5 3.5M33.2 33.2l3.5 3.5M36.7 11.3l-3.5 3.5M14.8 33.2l-3.5 3.5" />
+        <path d="M20 7 29 9l3 6 7 2-3 7 2 7-7 3-4 7-6-4-6-1 1-7-4-5 5-5-1-6 7-1Z" />
+        <path d="M21 13c3 5 4 9 3 13-1 5-1 10 1 15" />
       </svg>
     ),
   },
   {
     title: 'envio para todo brasil',
-    text: 'receba com segurança na sua casa.',
+    text: 'Receba com segurança na sua casa.',
     icon: (
       <svg viewBox="0 0 48 48" aria-hidden="true">
-        <path d="M8 15.5 24 8l16 7.5v18L24 41l-16-7.5v-18Z" />
-        <path d="M8 15.5 24 23l16-7.5M24 23v18" />
-        <path d="M16.2 11.8 32 19.2" />
-        <path d="M34.5 34.5c2.1-1.9 3.7-4.2 4.4-6.7" />
+        <path d="M6 13h24v21H6zM30 20h7l5 7v7H30z" />
+        <path d="M13 38a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM36 38a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" />
+        <path d="M30 27h12" />
       </svg>
     ),
   },
