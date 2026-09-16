e79aa1aa86fde2a247268e680486872ff5504438
agotrancoso-create <agotrancoso@gmail.com>
2026-09-16 09:44:17 -0300

    Aplica camada visual final da Ago

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import './ago-responsive-final.css';
 import './ago-final-device-audit.css';
 import './ago-ajuste-final-sessoes.css';
 import './ago-paleta-coesa-final.css';
+import './ago-direcao-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
