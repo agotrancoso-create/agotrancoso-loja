506c0743e96f66c4ad3e85df7e4ab973f5ddf7de
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 17:23:34 -0300

    design: ativa ultima camada visual integrada

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ import './ago-experiencia-total-2026.css';
 import './ago-oferta-checkout-legal-2026.css';
 import './ago-alinhamento-total-final-2026.css';
 import './ago-refinamento-visual-final-2026.css';
+import './ago-redesign-forca-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
