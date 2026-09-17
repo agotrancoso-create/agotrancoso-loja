7c12eda101443eb6a6fae2a780ef24a3d424029f
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 09:28:05 -0300

    style: torna identidade Bahia a ultima camada visual

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ import './ago-salvador-sertao-final.css';
 import './ago-alinhamento-textual-final-2026.css';
 import './ago-force-alinhamento-visual-2026.css';
 import './ago-catalogo-editorial-final-2026.css';
+import './ago-identidade-bahia-final-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
