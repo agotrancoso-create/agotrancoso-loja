f93889e0774475b1f0935707b834d10ae04f70f5
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 12:05:11 -0300

    style: aplica camada final de oferta e paginas legais

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ import './ago-direcao-designer-final-2026.css';
 import './ago-brasilidade-2026.css';
 import './ago-prompt-mestre-final-2026.css';
 import './ago-experiencia-total-2026.css';
+import './ago-oferta-checkout-legal-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
