1b38533063dec4de785290dc21b66e11bc1efa3a
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 11:02:20 -0300

    feat: aplica camada final de brasilidade na loja

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,7 @@ import './ago-acessibilidade-proporcao-final-2026.css';
 import './ago-ux-acessibilidade-final-2026.css';
 import './ago-copyright-centralizado-final.css';
 import './ago-direcao-designer-final-2026.css';
+import './ago-brasilidade-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
