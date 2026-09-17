cc6169df940cd5c81689aac6044cc03d7fd9a6dc
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 10:24:57 -0300

    style: centraliza copyright

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ import './ago-visite-alinhamento-force-2026.css';
 import './ago-alinhamento-global-2026.css';
 import './ago-acessibilidade-proporcao-final-2026.css';
 import './ago-ux-acessibilidade-final-2026.css';
+import './ago-copyright-centralizado-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
