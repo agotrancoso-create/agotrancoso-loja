68ff96c983b6a6461dc2178e861acbff291ef391
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 09:17:34 -0300

    style: aplica identidade nova no catalogo

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,7 @@ import './ago-social-floaters-final-2026.css';
 import './ago-salvador-sertao-final.css';
 import './ago-alinhamento-textual-final-2026.css';
 import './ago-force-alinhamento-visual-2026.css';
+import './ago-catalogo-editorial-final-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
