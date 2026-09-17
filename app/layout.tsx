e480d4256b8ef39a8446cc84ebbb940c674b0c1d
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 10:14:58 -0300

    style: melhora acessibilidade e proporção

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,7 @@ import './ago-identidade-bahia-final-2026.css';
 import './ago-responsive-unico-2026.css';
 import './ago-visite-alinhamento-force-2026.css';
 import './ago-alinhamento-global-2026.css';
+import './ago-acessibilidade-proporcao-final-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
