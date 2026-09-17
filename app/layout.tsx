f9382c77d68ec7eda63f92e23ece5b6a352407f9
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 09:05:37 -0300

    style: aplica alinhamento e paleta Salvador e sertao

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ import './ago-rodape-restaurado-2026.css';
 import './ago-design-system-2026.css';
 import './ago-ajuste-terroso-elegante-final-2026.css';
 import './ago-social-floaters-final-2026.css';
+import './ago-salvador-sertao-final.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
