56fdc05f6d85e7d0c36445f1a70e275bc7a0ec05
agotrancoso-create <agotrancoso@gmail.com>
2026-09-17 16:39:31 -0300

    perf: otimiza logo do cabecalho

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
       <header className="site-header sticky top-0 z-40">
         <div className="ago-container header-inner">
           <Link href="/" className="header-logo" aria-label="Agô Trancoso">
-            <Image src="/logo.png" alt="Agô Trancoso" width={72} height={72} className="h-12 w-12 object-contain" quality={100} unoptimized priority />
+            <Image src="/logo.png" alt="Agô Trancoso" width={72} height={72} sizes="48px" className="h-12 w-12 object-contain" quality={82} priority />
           </Link>
 
           <nav className="header-nav header-nav-desktop" aria-label="Navegação principal">
