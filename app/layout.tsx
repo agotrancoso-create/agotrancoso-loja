0c8bc2639dda4b84308189b3afb1e0438e347ca7
agotrancoso-create <agotrancoso@gmail.com>
2026-09-18 00:34:03 -0300

    style: aplica camada final de direção da Agô

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ import './ago-unificacao-humana-2026.css';
 import './ago-tipografia-rodape-refinado-2026.css';
 import './ago-carrinho-premium-final-2026.css';
 import './ago-redesign-clean-2026.css';
+import './ago-forca-verdadeira-2026.css';
 import { CartProvider } from '@/context/CartContext';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
