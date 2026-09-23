cb9133243308eac4822495169a9edde4910792b1
agotrancoso-create <agotrancoso@gmail.com>
2026-09-23 19:50:34 -0300

    Aproxima beneficios da referencia com fonte do site

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -15,8 +15,8 @@ const benefits = [
     icon: 'brazil',
   },
   {
-    title: 'sem fronteiras',
-    text: 'uma lembrança para qualquer lugar.',
+    title: 'SEM FRONTEIRAS',
+    text: 'Uma lembrança para qualquer lugar.',
     icon: 'world',
   },
 ] as const;
@@ -24,10 +24,10 @@ const benefits = [
 type BenefitIconType = (typeof benefits)[number]['icon'];
 
 const iconProps = {
-  viewBox: '0 0 64 64',
+  viewBox: '0 0 120 120',
   fill: 'none',
   stroke: 'currentColor',
-  strokeWidth: 1.45,
+  strokeWidth: 1.65,
   strokeLinecap: 'round' as const,
   strokeLinejoin: 'round' as const,
   'aria-hidden': true,
@@ -37,12 +37,12 @@ function BenefitIcon({ type }: { type: BenefitIconType }) {
   if (type === 'handmade') {
     return (
       <svg {...iconProps}>
-        <path d="M18 45c-2.8-5.5-2.9-12.4 0-18.1l2.9-5.7c.8-1.6 3.1-1.3 3.5.4l2.4 8.9" />
-        <path d="M23 31.5 21.2 20c-.3-2 1-3.8 3-4.2 2-.4 3.8.8 4.3 2.7l2.2 9.4" />
-        <path d="m30.7 29.5-.8-11.3c-.1-2 1.4-3.7 3.4-3.9 2-.1 3.7 1.3 3.9 3.3l1 10.8" />
-        <path d="m38.2 27.9.2-7.1c.1-2 1.7-3.5 3.7-3.4 2 .1 3.5 1.7 3.5 3.7v8.1c0 7.2-5 13.1-11.9 14.5l-5.7 1.1c-4 .8-8-1.1-10-4.7" />
-        <path d="M21 49h21" />
-        <path d="M32 23.8c-2.2-4.5-9.6-3.1-9.6 1.9 0 3.7 5.7 6.9 9.6 10.3 3.9-3.4 9.6-6.6 9.6-10.3 0-5-7.4-6.4-9.6-1.9Z" />
+        <path d="M35 89c-8-12-11-26-8-39l4-17c1-4 7-5 9-1l6 19" />
+        <path d="M46 52 41 28c-1-5 3-9 8-9 4 0 7 3 8 7l6 24" />
+        <path d="m63 49-2-27c0-5 3-8 8-8 4 0 7 3 7 7l2 27" />
+        <path d="m78 48 2-21c0-4 4-7 8-6 4 0 7 4 6 8l-1 25c-1 16-11 28-26 31l-7 1c-8 1-16-3-20-10" />
+        <path d="M30 98h38" />
+        <path d="M60 33c-8-13-27-8-27 5 0 11 16 19 27 29 11-10 27-18 27-29 0-13-19-18-27-5Z" />
       </svg>
     );
   }
@@ -50,13 +50,13 @@ function BenefitIcon({ type }: { type: BenefitIconType }) {
   if (type === 'ceramic') {
     return (
       <svg {...iconProps}>
-        <path d="M22 14h20" />
-        <path d="M24 14v8.6c0 3.2-1.1 5.6-2.7 8.1-1.7 2.5-2.7 5-2.7 8.2A9.4 9.4 0 0 0 28 48h8a9.4 9.4 0 0 0 9.4-9.1c0-3.2-1-5.7-2.7-8.2-1.6-2.5-2.7-4.9-2.7-8.1V14" />
-        <path d="M21.5 24.2h21" />
-        <path d="M26.5 10h11" />
-        <path d="M24 32.8c4.5 2.6 11.5 2.6 16 0" />
-        <path d="M25.5 39.2c3.9 1.7 7.9 1.7 11.8 0" />
-        <path d="M27 18.4c1.1.8 2.2.8 3.3 0M34 18.4c1.1.8 2.2.8 3.3 0" />
+        <path d="M46 20h28" />
+        <path d="M49 20v12c0 6-2 10-6 16-4 6-7 12-7 19 0 12 9 21 20 21h12c11 0 20-9 20-21 0-7-3-13-7-19-4-6-6-10-6-16V20" />
+        <path d="M43 38h34" />
+        <path d="M48 15h24" />
+        <path d="M45 56c9 5 21 5 30 0" />
+        <path d="M45 71c8 3 22 3 30 0" />
+        <path d="M49 28c2 2 5 2 7 0m8 0c2 2 5 2 7 0" />
       </svg>
     );
   }
@@ -64,27 +64,25 @@ function BenefitIcon({ type }: { type: BenefitIconType }) {
   if (type === 'brazil') {
     return (
       <svg {...iconProps}>
-        <path d="M27.6 8.5 33 10l4.3-1.2 3 3.4 5 1.9 1.6 4.6-2.1 4.1 1 4.5-3.2 4.2-4 2-1 5.4-3.3 4-2.7 4.9-4.6-2.2-4.2.8-2.4-3.8-4.3-1.9-1.9-4.7 1.5-4-1.3-4.1 2.9-4-.3-4.7 3.6-2.5 1.2-4.2 3.8-1.7Z" />
-        <path d="m23 19.3 7.9 7.4 10.2-2" />
-        <path d="M30.9 27.1 32 39.4" />
+        <path d="M47 16 60 20l10-4 9 9 16 6 5 13-7 13 2 13-10 12-12 6-3 13-10 8-8 15-14-7-13 3-8-12-13-7-6-15 5-12-5-13 9-11-1-14 11-7 4-13 13-5Z" />
+        <path d="m40 45 21 19 27-8" />
+        <path d="m61 64 3 31" />
       </svg>
     );
   }
 
-  if (type === 'world') {
-    return (
-      <svg {...iconProps}>
-        <circle cx="32" cy="32" r="18" />
-        <path d="M14 32h36" />
-        <path d="M32 14c5.4 4.8 8.4 11 8.4 18S37.4 45.2 32 50" />
-        <path d="M32 14c-5.4 4.8-8.4 11-8.4 18S26.6 45.2 32 50" />
-        <path d="M18.2 23.3c4.3 2 8.8 3 13.8 3s9.5-1 13.8-3" />
-        <path d="M18.2 40.7c4.3-2 8.8-3 13.8-3s9.5 1 13.8 3" />
-      </svg>
-    );
-  }
-
-  return null;
+  return (
+    <svg {...iconProps}>
+      <circle cx="60" cy="60" r="38" />
+      <path d="M22 60h76" />
+      <path d="M60 22c11 10 17 23 17 38s-6 28-17 38" />
+      <path d="M60 22c-11 10-17 23-17 38s6 28 17 38" />
+      <path d="M30 42c9 4 19 6 30 6s21-2 30-6" />
+      <path d="M30 78c9-4 19-6 30-6s21 2 30 6" />
+      <path d="M27 35c12-12 29-18 46-14 10 2 18 7 24 15" />
+      <path d="M93 85c-12 12-29 18-46 14-10-2-18-7-24-15" />
+    </svg>
+  );
 }
 
 export default function Benefits() {
