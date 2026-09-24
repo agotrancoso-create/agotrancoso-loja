445f77082a82454f6c40f29469e2aa3d2fa32235
agotrancoso-create <agotrancoso@gmail.com>
2026-09-23 23:39:41 -0300

    Remove efeitos globais que competiam com produtos

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import Footer from '@/components/Footer';
 import CartDrawer from '@/components/CartDrawer';
 import SocialFloaters from '@/components/SocialFloaters';
 import FirstPurchaseOffer from '@/components/FirstPurchaseOffer';
-import InteractiveEnhancements from '@/components/InteractiveEnhancements';
 import { SITE_DOMAIN } from '@/lib/config';
 
 const manrope = Manrope({
@@ -83,7 +82,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <MarketingAnalytics />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
         <CartProvider>
-          <InteractiveEnhancements />
           <Header />
           <main id="conteudo-principal">{children}</main>
           <Footer />
