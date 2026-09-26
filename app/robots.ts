422975d52dbed28c20a74dbf514151af1e1f426c
agotrancoso-create <agotrancoso@gmail.com>
2026-09-26 09:25:16 -0300

    Refina robots e referência do sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -3,9 +3,14 @@ import { SITE_DOMAIN } from '@/lib/config';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/checkout', '/confirmacao', '/api/'] }],
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/checkout', '/confirmacao', '/api/'],
+      },
+    ],
     sitemap: `${SITE_DOMAIN}/sitemap.xml`,
+    host: SITE_DOMAIN,
   };
 }
-
-// Deploy marker: publicar versao refinada da Agô em 2026-09-17.
